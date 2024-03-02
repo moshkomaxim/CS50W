8ca3df8dea278b7c611dea7bc514b98e914117d8
moshkomaxim <moshkomaxim@gmail.com>
2024-03-02 13:52:21 -0400

    Commit

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, Listing, Bids, Category
 # Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    filter_horizontal = ("bids", )
+    
+admin.site.register(User)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bids)
+admin.site.register(Category)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_bids")
+    bid = models.IntegerField()
+
+class Listing(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+    #image = models.ImageField()
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listed_by")
+    bids = models.ManyToManyField(Bids, null=True, blank=True, related_name="bids")
+
